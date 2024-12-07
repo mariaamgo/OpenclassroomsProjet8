e97cd0221459d28a61f7857223659fcc0c9616b0
Maria Amigo <maria.amigo.07@gmail.com>
2024-12-07 21:10:24 +0100

    Réajustement de app.js (suppression des lignes de code inutiles, ajout de la route mail.js

--- app.js
+++ app.js
@@ -1,75 +1,18 @@
-var createError = require('http-errors');
 var express = require('express');
-var nodemailer = require('nodemailer');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var compression = require('compression');
 var app = express();
+var mailRouter = require('./routes/mail');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+app.use(compression());
+app.use(express.static('public'));
+app.use('/', mailRouter);
 
 app.get('/', (req, res) =>{
   res.sendFile(__dirname + 'public')
 })
 
-app.post('/', (req, res)=>{
-  console.log(req.body);
 
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'maria.amigo.07@gmail.com',
-      pass: 'qqwl pwnm ukar rfcz'
-    }
-  })
-
-  const mailOptions = {
-    from: 'maria.amigo.07@gmail.com',
-    replyTo: req.body.email,
-    to: 'maria.amigo.07@gmail.com',
-    subject: 'Message provenant de mariaamigo.fr de : ' + req.body.name,
-    text: req.body.message
-  }
-
-  transporter.sendMail(mailOptions, (error, info)=>{
-    if(error){
-      console.log(error);
-      res.send('error');
-    }else{
-      console.log('Email envoyé : ' + info.response);
-      res.send('success');
-    }
-  })
-})
 module.exports = app;
